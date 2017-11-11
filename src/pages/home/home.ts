313b36f875c769723538621a6082f1dc4dfe8162
David Auger <deayqf@mail.missouri.edu>
2017-11-10 18:43:59 -0600

    added UI feedback, form validation, and comments

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,13 @@
+/** David 11/10/17
+ * Ready for DB connection.
+ * Functions for data retrieval and validation are set up.
+ * Some UI feedback is set up:
+ *	* Inputs are empty or invalid
+ *	* There account has been created if they registered
+**/
+
 import { Component } from '@angular/core';
-import { NavController, ModalController, NavParams } from 'ionic-angular';
+import { NavController, ModalController, NavParams, AlertController, ToastController } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
 
 @Component({
@@ -8,19 +16,51 @@ import { RegisterPage } from '../register/register';
 })
 export class HomePage {
 
-	usrnm: string
-	psswd: string
+	//we could make this an object -> login_info: any
+	usrnm: string = ''
+	psswd: string = ''
 
-	constructor( public navCtrl: NavController, private modalCtrl: ModalController ) {
+	constructor( public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController, private toastCtrl: ToastController ) {
 
 	}
 
 	//called when login button is pressed
 	attemptLogin(): void 
 	{
-		console.log( 'Login' )
-		console.log( 'Username: ' + this.usrnm )
-		console.log( 'Password: ' + this.psswd )
+		if( this.inputisEmpty( this.usrnm ) || this.inputisEmpty( this.psswd ) ) //if either are empty or fail regex test, alert user 
+		{
+			this.showAlert( 'Input Error', 'All inputs are required' )
+		}
+		// else { login() }
+	}
+
+	//tells the user whats wrong with the input fields
+	showAlert( title, msg ): void
+	{
+		let alert = this.alertCtrl.create(
+			{
+				title: title,
+				subTitle: msg,
+				buttons: [ 'Ok' ]	
+			}
+		)
+		alert.present()
+	}
+
+	//tells the user their profile has been created and prompts
+	//them to login
+	showRegistrationConfirm(): void
+	{
+		let toast = this.toastCtrl.create(
+			{
+				message: 'User successfully added, now just login!',
+				duration: 5000,
+				position: 'middle',
+				showCloseButton: true,
+				closeButtonText: 'Ok'
+			}
+		)
+		toast.present()
 	}
 
 	//called when register button is pressed
@@ -28,6 +68,32 @@ export class HomePage {
 	{
 		console.log( 'Register' )
 		let modal = this.modalCtrl.create( RegisterPage )
+
+		modal.onDidDismiss( data => {
+			if( data.usrnm != '' && data.psswd != '' ) //if data is empty then the registration was cancelled
+			{
+				this.usrnm = data.usrnm
+				this.psswd = data.psswd
+				this.showRegistrationConfirm()
+			}
+		})
+
 		modal.present()
+	}
+
+	//checks if input is okay to verify
+	inputisEmpty( str ): boolean
+	{
+		/** /^(?=.*[\d])(?=.*[!@#$%^&*])[\w!@#$%^&*]{6,16}$/
+		 * This will change at some point to be more secure
+		 * but for now it only allows letters (a-zA-z),
+		 * numbers, and [ !, @, #, $, %, ^, &, * ].
+		 * 
+		 * Eventually it will require a certain number of
+		 * characters that contains capital letters, 
+		 * special chars, etc.
+		**/
+		let re = /^[\w!@#$%^&*]+$/
+		return !re.test( str )
 	}
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,15 @@
+/** David 11/10/17
+ * Ready for DB connection.
+ * Functions for data retrieval and validation are set up.
+ * Some UI feedback for the user has been set up:
+ *	* Inputs are empty
+ *	* Invalid email
+ *	* Passwords don't match
+ * Registration modal is set up with cancel option.
+**/
+
 import { Component } from '@angular/core';
-import { NavParams, ViewController } from 'ionic-angular';
+import { NavParams, ViewController, AlertController } from 'ionic-angular';
 
 @Component({
 	selector: 'page-register',
@@ -7,25 +17,75 @@ import { NavParams, ViewController } from 'ionic-angular';
 })
 export class RegisterPage {
 
-	usrnm: string
-	email: string
-	psswd: string
-	cnfrm: string
+	usrnm: string = ''
+	email: string = ''
+	psswd: string = ''
+	cnfrm: string = ''
 
-	constructor( private navParams: NavParams, private viewCtrl: ViewController ) {
+	constructor( private navParams: NavParams, private viewCtrl: ViewController, private alertCtrl: AlertController ) {
 	}
 
 	attemptRegistration(): void
 	{
-		console.log( 'Username: ' + this.usrnm )
-		console.log( 'Email: ' + this.email )
-		console.log( 'Password: ' + this.psswd )
-		console.log( 'Confirm Password: ' + this.cnfrm )
+		let credentials = { usrnm: this.usrnm, email: this.email, psswd: this.psswd, cnfrm: this.cnfrm } //make object from login info for simpler access when validating
+		var flag: boolean = true
+
+		Object.keys( credentials ).forEach( key => {
+			if( this.isEmpty( credentials[ key ] ) )
+			{
+				flag = false
+			}
+		})
+
+		if( !flag )
+		{
+			this.showAlert( 'Invlalid Inputs', 'All inputs are required' )
+		}
+		else if( !this.verifyEmail() )
+		{
+			this.showAlert( 'Email Error', "Valid emails must include an '@' followed by the domain" )
+		}
+		else if( this.psswd != this.cnfrm )
+		{
+			this.showAlert( 'Password Error', 'Passwords must match' )
+		}
+		else
+		{
+			//pass new username and password back to login for immediate login
+			this.viewCtrl.dismiss( { usrnm: this.usrnm, psswd: this.psswd } )
+		}
 	}
 
+	showAlert( title, msg ): void
+	{
+		let alert = this.alertCtrl.create(
+			{
+				title   : title,
+				subTitle: msg,
+				buttons : [ 'Ok' ]
+			}
+		)
+		alert.present()
+	}
+
+	verifyEmail(): boolean
+	{
+		//confirms the email consists of 'string'-'@'-'string'-'.'-'string'
+		let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		return re.test( this.email )
+	}
+
+	isEmpty( str ): boolean
+	{
+		//fails if even a space is in the input
+		let re = /^$/
+		return re.test( str )
+	}
+
+	//called when the user presses cancel
 	dismiss(): void
 	{
-		this.viewCtrl.dismiss()
+		this.viewCtrl.dismiss( { usrnm: '', psswd: '' } )
 	}
 
 }
