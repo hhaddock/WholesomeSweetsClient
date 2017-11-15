67f18c590c86781399ecf5d2a47e9de38bd6fd2f
David Auger <deayqf@mail.missouri.edu>
2017-11-15 16:52:47 -0600

    idk

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -51,7 +51,7 @@ export class HomePage
 			pass: this.psswd
 		}
 
-		this.httpCtrl.post( 'http://localhost:3000/api/login', post_params, options )
+		this.httpCtrl.post( 'http://localhost:8100/api/login', post_params, options )
 		.subscribe( data => {
 			console.log( data[ '_body' ] )
 		}, error => {
